5b2c5391f0a53d884357103ae772f2c5db0ed029
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-05-06 11:29:46 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -124,7 +124,7 @@ async def echo(bot, update):
             chat_id=update.chat.id,
             text=f'ᴘʀᴏᴄᴇssɪɴɢ ʏᴏᴜʀ ʟɪɴᴋ ⌛',
             disable_web_page_preview=True,
-            reply_to_message_id=update.message.id
+            Message=update.id
           )
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
@@ -149,7 +149,7 @@ async def echo(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-            reply_to_message_id=update.message_id,
+            Message=update.id,
             parse_mode="html",
             disable_web_page_preview=True
         )
@@ -260,7 +260,7 @@ async def echo(bot, update):
             text=Translation.FORMAT_SELECTION.format(Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
             parse_mode="html",
-            reply_to_message_id=update.message_id
+            Message=update.id
         )
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
@@ -282,7 +282,7 @@ async def echo(bot, update):
             text=Translation.FORMAT_SELECTION,
             reply_markup=reply_markup,
             parse_mode="html",
-            reply_to_message_id=update.message_id
+            Message=update.id
         )
 
 
