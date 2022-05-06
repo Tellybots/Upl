3a85d90da52788fe76ecfbe8b27e131419693b85
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-05-06 09:58:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Tellybots/Uploader-Bot /Uploader-Bot
-cd /Uploader-Bot
+git clone https://github.com/Tellybots/Uploader_X_Bot /Uploader_x_Bot
+cd /Uploader_X_Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
 python3 bot.py
